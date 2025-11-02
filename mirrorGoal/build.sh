6bd6a2a91126bf6c16a5b6509adcad5d52a43b95
shivansh <shivanshvarshney45@gmail.com>
2025-11-03 01:35:19 +0530

    Add start.sh for all-in-one deployment and update deployment guide

--- mirrorGoal/build.sh
+++ mirrorGoal/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='${DJANGO_SUPERUSER_EMAIL}').exists() or User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')"
